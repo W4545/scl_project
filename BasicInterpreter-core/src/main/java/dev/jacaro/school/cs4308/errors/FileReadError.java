3a4b8877157d796267ebee39d9679cb4aba55f7a
W4545 <w4545@jacaro.dev>
2021-10-15 14:56:13 -0400

    Fix typo

--- BasicInterpreter-core/src/main/java/dev/jacaro/school/cs4308/errors/FileReadError.java
+++ BasicInterpreter-core/src/main/java/dev/jacaro/school/cs4308/errors/FileReadError.java
@@ -2,6 +2,6 @@ package dev.jacaro.school.cs4308.errors;
 
 public class FileReadError extends Exception {
     public FileReadError(String file, Throwable cause) {
-        super(String.format("An error occured while loading the file %s", file), cause);
+        super(String.format("An error occurred while loading the file %s", file), cause);
     }
 }
