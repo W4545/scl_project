46a38c3b3d9c49b36e0ffaa317b765ab264b4baa
W4545 <w4545@jacaro.dev>
2021-11-10 20:27:48 -0500

    Fixed IDGenerator.kt to use IDManager for ID generation

--- BasicInterpreter-parser/src/main/java/dev/jacaro/school/cs4308/parser/generator/IDGenerator.kt
+++ BasicInterpreter-parser/src/main/java/dev/jacaro/school/cs4308/parser/generator/IDGenerator.kt
@@ -1,8 +1,11 @@
 package dev.jacaro.school.cs4308.parser.generator
 
+import dev.jacaro.school.cs4308.ids.IDManager
 import dev.jacaro.school.cs4308.values.ID
 import dev.jacaro.school.cs4308.structure.Token
 
 object IDGenerator : TokenGenerator<ID>(Token.ID, {
-    ID(value)
+    IDManager.ids.getOrPut(value) {
+        ID(value)
+    }
 })
