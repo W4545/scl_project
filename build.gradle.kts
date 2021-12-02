78e4b4b18022bda0340d9593b618a0350323a30a
W4545 <w4545@jacaro.dev>
2021-12-02 11:07:26 -0500

    Add Dakka for documentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
 plugins {
+    id("org.jetbrains.dokka") version "1.6.0"
+}
 
+tasks.dokkaGfmMultiModule.configure {
+    outputDirectory.set(buildDir.resolve("dokkaCustomMultiModuleOutput"))
 }
 
 allprojects {
+    apply(plugin = "org.jetbrains.dokka")
+
     repositories {
         mavenCentral()
     }
